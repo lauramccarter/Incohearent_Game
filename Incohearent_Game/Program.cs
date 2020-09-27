84d55b684d439f1564bfeb8efd4dd8ef0f63214a
lauramccarter <71936540+lauramccarter@users.noreply.github.com>
2020-09-27 10:44:27 -0700

    completed code

--- Incohearent_Game/Program.cs
+++ Incohearent_Game/Program.cs
@@ -16,26 +16,35 @@ namespace Incohearent_Game
             foreach( Card card in cards)
             {
                 Console.WriteLine(Environment.NewLine + card.Question);
-                string input = Console.ReadLine();
+                string input = ""; 
 
                 while (input != "g")
                 {
+                    input = Console.ReadLine();
+
                     if (input == card.Answer)
                     {
                         // user picked the right answer
+                        Console.WriteLine("You got it right!");
+                        input = "g";
                     }
                     else if (input == "h")
                     {
                         // user wants a hint
+                        Console.WriteLine("Hint: " + card.Hint);
                     }
                     else if (input == "g")
                     {
                         // user gives up
+                        Console.WriteLine("Answer: " + card.Answer);
+                        break;
                     }
                     else
                     {
                         // user entered wrong answer
+                        Console.WriteLine("Wrong. Try again");
                     }
+
                 }
             }
         }
@@ -48,7 +57,11 @@ namespace Incohearent_Game
 
             // Write a for loop here to loop through all the Card objects in cars, create
             // card objects and add them to the list
-           
+           foreach (string line in lines)
+            {
+                Card mycard = new Card(line);
+                cards.Add(mycard);
+            }
 
             return cards;
         }
@@ -61,7 +74,11 @@ namespace Incohearent_Game
 
             public Card(string line)
             {
-               // write code here to get question, hint, and answer strings assigned
+                // write code here to get question, hint, and answer strings assigned
+                string[] mystring = line.Split(';');
+                Question = mystring[0];
+                Hint = mystring[1];
+                Answer = mystring[2];
             }
         }
 
